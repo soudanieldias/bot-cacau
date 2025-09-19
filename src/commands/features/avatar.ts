2e45351e077a7a1275e7f1bc1d4f8c6cae375665
soudanieldias <soudanieldias@hotmail.com>
2025-09-18 23:32:50 -0300

    feat: add avatar + serverinfo commands

---
+++ src/commands/features/avatar.ts
@@ -0,0 +1,34 @@
+import { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import { CommandData } from '../../types';
+
+export default (): CommandData => ({
+  data: new SlashCommandBuilder()
+    .setName('avatar')
+    .setDescription('Mostra o avatar do usuário')
+    .setDefaultMemberPermissions(PermissionFlagsBits.UseApplicationCommands)
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('O usuário cujo avatar você quer ver')
+        .setRequired(false)
+    ),
+  categories: ['features'],
+
+  async execute(_client, interaction) {
+    try {
+      const user = interaction.options.getUser('user') || interaction.user;
+
+      const avatarUrl = user.displayAvatarURL({ size: 512 });
+
+      const embed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle(`${user.username}'s Avatar`)
+        .setImage(avatarUrl);
+
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  
+});
---
+++ src/commands/features/serverinfo.ts
@@ -0,0 +1,59 @@
+import {
+    ChatInputCommandInteraction,
+    Client,
+    EmbedBuilder,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+} from 'discord.js';
+import { CommandData } from '../../types';
+
+export default (): CommandData => ({
+  data: new SlashCommandBuilder()
+    .setName('serverinfo')
+    .setDescription('Mostra um Embed com as informações sobre o Servidor/Guild')
+    .setDefaultMemberPermissions(PermissionFlagsBits.UseApplicationCommands),
+  categories: ['help'],
+
+  execute: async (client:Client<true>, interaction: ChatInputCommandInteraction) => {
+    try {
+      const guildMembers = await interaction.guild!.members.fetch();
+      const membersCount = guildMembers.filter(member => !member.user.bot).size;
+      if (interaction.guild && client.user && interaction.isRepliable()) {
+        const guildEmbed = new EmbedBuilder()
+          .setColor('#ffffff')
+          .setTitle('Informações do Servidor:')
+          .addFields(
+            { name: 'Nome do Servidor: ', value: `${interaction.guild.name}` },
+            {
+              name: 'Dono do Servidor: ',
+              value: `<@!${interaction.guild.ownerId}>`,
+            },
+            { name: 'Membros no Servidor: ', value: `${membersCount}` },
+            {
+              name: 'Bots no Servidor: ',
+              value: `${(guildMembers.size - membersCount) || 0}`,
+            },
+            {
+              name: 'Location: ',
+              value: `${interaction.guild?.preferredLocale}`,
+              inline: true,
+            },
+            {
+              name: 'Created',
+              value: `${interaction.guild.createdAt.toLocaleString()}`,
+              inline: true,
+            }
+          )
+          .setTimestamp()
+          .setFooter({
+            text: `${client.user.username}`,
+            iconURL: `${client.user.avatarURL()}`,
+          });
+
+        interaction.reply({ embeds: [guildEmbed] });
+      }
+    } catch (error) {
+      console.error('[SERVER INFO]', error);
+    }
+  },
+});
